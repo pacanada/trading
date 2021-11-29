96ebfbc00f89546f6fc380a6dc8356ed0abc6317
pacanada <pereirapcanada@gmail.com>
2021-11-29 19:10:30 +0100

    bunch of stuff

---
+++ src/core/backtest/setup.py
@@ -0,0 +1,23 @@
+from Cython.Distutils import build_ext
+from distutils.core import setup
+from distutils.extension import Extension
+import numpy
+
+import os
+os.environ['CC'] = 'g++'
+os.environ['CXX'] = 'g++'
+
+ext_modules = [
+    Extension(
+        name="backtesting_cpp",
+        sources=["backtesting_cpp.pyx"],
+        extra_compile_args=["-Wno-cpp", "-Wno-unused-function", "-O2", "-march=native", '-stdlib=libc++', '-std=c++11'],
+        extra_link_args=["-O2", "-march=native", '-stdlib=libc++'],
+        language="c++",
+        include_dirs=[".", numpy.get_include()],
+    )
+]
+
+setup(
+    name="sum", ext_modules=ext_modules, cmdclass={"build_ext": build_ext}
+)
